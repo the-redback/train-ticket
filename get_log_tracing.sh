2b3f51f9b8ea93c44bc3eef2bf77af43df09fa96
the-redback <maruf.2hin@gmail.com>
2022-04-12 04:47:16 -0500

    Updated logs.

--- get_log_tracing.sh
+++ get_log_tracing.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -euo pipefail
 
-for i in $(kubectl get po | tail -n +2 | awk '{ print $1 }')
+for i in $(kubectl get deploy | tail -n +2 | awk '{ print $1 }')
 do
-    echo "Pod Name: $i "
-    kubectl logs $i > ./logs/kubernetes-istio/application-log/$i.log 2>&1
-    kubectl logs $i istio-proxy > ./logs/kubernetes-istio/tracing-log/$i.log 2>&1
+    echo "Deployment Name: $i "
+    kubectl logs deploy/$i > ./logs/kubernetes-istio/application-log/$i.log 2>&1
+    kubectl logs deploy/$i istio-proxy > ./logs/kubernetes-istio/tracing-log/$i.default.log 2>&1
 done
